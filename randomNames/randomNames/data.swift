cb1de0685393eed9ff3cb4087d63d5fa644b7fa9
Faisal Almutairi <faisal_almutairi@Faisals-MacBook-Air.local>
2022-08-24 02:10:42 +0300

    random names assignment

---
+++ randomNames/randomNames/data.swift
@@ -0,0 +1,10 @@
+//
+//  data.swift
+//  randomNames
+//
+//  Created by Faisal Almutairi on 25/01/1444 AH.
+//
+
+import Foundation
+
+var names :[String] = ["faisal","fahad","khaled","yasser","rayan","mohammed","rashed"]
